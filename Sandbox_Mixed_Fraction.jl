32dc59f214f112dc8167aacfa3b868bc310bb62d
ConstLike <constlike@gmail.com>
2021-01-20 02:10:36 +0700

    Output of mixed fraction and Testing/
    Added output of mixed fraction from improper fraction.
    Testing

--- Sandbox_Mixed_Fraction.jl
+++ Sandbox_Mixed_Fraction.jl
@@ -15,6 +15,8 @@ struct Ratio <: Real
 	end
 end
 
+"""  Mixed_Frac(r, k)
+ruturn an integer part 'r' of the improper fraction 'r' """
 struct Mixed_Frac <: Real
     num::Int
 	den::Int
@@ -37,6 +39,9 @@ end
 begin
     numer(r::Ratio) = r.numer
     denom(r::Ratio) = r.denom
+    num(r::Mixed_Frac) = r.num
+    den(r::Mixed_Frac) = r.den
+    ing(r::Mixed_Frac) = r.ing
 
     function Base.:+(x::Ratio, y::Ratio)
     	Ratio(numer(x) * denom(y) + numer(y) * denom(x),
@@ -84,6 +89,10 @@ begin
         Ratio(numer(x) * y,
               denom(x))
     end
+    function Base.:^(x::Ratio, y::Integer)
+        Ratio(numer(x)^y,
+              denom(x)^y)
+    end
 
     function Base.:/(x::Ratio, y::Ratio)
     	Ratio(numer(x) * denom(y),
@@ -117,8 +126,35 @@ begin
     end
 
     Base.show(io::IO, r::Ratio) = print(io, numer(r), '/', denom(r))
+    Base.show(io::IO, r::Mixed_Frac) =
+        (ing(r) == 0 ?
+            (num(r) == 0 ?
+                print(io, num(r))
+                : print(io, num(r), '/', den(r))
+            )
+            : (num(r) == 0 ?
+                print(io, ing(r))
+                : print(io, ing(r), " and ", num(r), '/', den(r))
+            )
+        )
 end
 
-Ratio(1, 2) + Ratio(3, 4)
-Ratio(1, -2)
-Ratio(5, -2) + Ratio(1, -2)
+""" Testing:         """
+one_half = Ratio(1, 0) # WRONG: Argument of 'denominator' cannot be zero
+one_half = Ratio(1, 2) # = 1/2
+one_third = Ratio(1, 3) # = 1/3
+minus_quarter = Ratio(1, -4) # = -1/4
+
+one_half / minus_quarter # = -2/1
+Mixed_Frac(one_half / minus_quarter) # = -2
+
+Mixed_Frac(one_half * one_third / minus_quarter) # = -2/3
+Mixed_Frac(one_half - one_third / minus_quarter) # = 1 and 5/6
+Mixed_Frac(one_half * one_third * minus_quarter) # = -1/24
+Mixed_Frac(48 * one_half * one_third * minus_quarter) # = -2
+Mixed_Frac(3 * one_half * one_third) # = 1/2
+Mixed_Frac(3 / one_half * one_third) # = 2
+Mixed_Frac(3 + one_half * one_third) # 3 and 1/6
+Mixed_Frac(one_half - one_half) # 0
+Mixed_Frac(one_third ^ 3) # = 1/27
+Mixed_Frac(abs(minus_quarter)) # = 1/4
